df79930909473ff93bd92fbaf33581d66eeae02d
Al Lukman <allukman1802@gmail.com>
2023-05-22 10:25:22 +0700

    feature: add card components

--- src/components/noted-item/NotedItem.js
+++ src/components/noted-item/NotedItem.js
@@ -2,35 +2,110 @@ import React from "react";
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const BlogItem = ({ item, navigation, deleteBlogPost }) => {
+const NotedItem = ({ item, navigation, deleteBlogPost }) => {
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("Detail", { id: item.id, title: item.title })}>
+    <TouchableOpacity
+      style={{ flex: 1 }}
+      onPress={() =>
+        navigation.navigate("Detail", { id: item.id, title: item.title })
+      }
+    >
       <View style={styles.row}>
-        <Text style={styles.title}>{item.title} - {item.id}</Text>
-        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-          <Feather style={styles.icon} name="trash" />
-        </TouchableOpacity>
+        <View style={styles.card}>
+          <View style={styles.container}>
+            <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+            <Text style={styles.description} numberOfLines={2}>{item.content}</Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.trashButton}
+            onPress={() => deleteBlogPost(item.id)}
+          >
+            <Feather style={styles.icon} name="trash" />
+          </TouchableOpacity>
+        </View>
       </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  row: {
+  container: {
+    flex: 1,
+  },
+  card: {
+    flex: 1,
+    alignSelf: "stretch",
     flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 4,
-    paddingVertical: 20,
-    borderBottomWidth: 1,
-    borderColor: "gray",
+    backgroundColor: "#ffffff",
+    borderRadius: 8,
+    padding: 16,
+    shadowColor: "#000000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 4,
+    elevation: 4,
+    margin: 8,
   },
   title: {
     fontSize: 18,
-    fontWeight: "500",
+    fontWeight: "bold",
+    marginBottom: 8,
+    marginRight: 16
+  },
+  description: {
+    fontSize: 16,
+    color: "#666666",
   },
   icon: {
     fontSize: 24,
   },
+  trashButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    padding: 8,
+  },
 });
 
-export default BlogItem;
+export default NotedItem;
+
+// const NotedItem = ({ item, navigation, deleteBlogPost }) => {
+//     return (
+//       <View style={styles.card}>
+//         <Text style={styles.title}>Card Title</Text>
+//         <Text style={styles.description}>Card Description</Text>
+//       </View>
+//     );
+//   };
+
+//   const styles = StyleSheet.create({
+//     card: {
+//       backgroundColor: "#ffffff",
+//       borderRadius: 8,
+//       padding: 16,
+//       shadowColor: "#000000",
+//       shadowOpacity: 0.2,
+//       shadowOffset: {
+//         width: 0,
+//         height: 2,
+//       },
+//       shadowRadius: 4,
+//       elevation: 4,
+//       margin: 8
+//     },
+//     title: {
+//       fontSize: 18,
+//       fontWeight: "bold",
+//       marginBottom: 8,
+//     },
+//     description: {
+//       fontSize: 16,
+//       color: "#666666",
+//     },
+//   });
+
+//   export default NotedItem;
